008b7420f62512eddf2bf79e93156a1fdae1efc0
Simon <simoblas08@gmail.com>
2026-01-09 19:46:37 -0300

    ajustes visuales para card experience

--- src/components/layout/ExperienceSection.tsx
+++ src/components/layout/ExperienceSection.tsx
@@ -20,7 +20,7 @@ const ExperienceSection = () => {
     });
   };
   return (
-    <section id="experience" className="py-20 px-6 max-w-4xl mx-auto ">
+    <section id="experience" className="py-20 px-6 max-w-5xl mx-auto ">
       <h2 className="text-3xl font-bold mb-16 text-center w-fit mx-auto italic">
         Experience
       </h2>
@@ -32,22 +32,22 @@ const ExperienceSection = () => {
             className="relative flex items-center justify-between md:justify-normal md:odd:flex-row-reverse group"
           >
             {/* punto y línea de tiempo */}
-            <div className="flex items-center justify-center w-10 h-10 rounded-full border border-slate-700 bg-slate-900 shadow shrink-0 md:order-1 md:group-odd:-translate-x-[55%] md:group-even:translate-x-[55%]">
+            <div className="flex items-center justify-center w-10 h-10 rounded-full border border-slate-700 bg-slate-900 shadow shrink-0 md:order-1 md:group-odd:-translate-x-[72%] md:group-even:translate-x-[72%]">
               <div className="w-2 h-2 bg-green-200 rounded-full shadow-[0_0_8px_rgba(34,211,238,0.8)]"></div>
             </div>
             {/* Contenido de la tarjeta */}
-            <div className="w-[calc(100%-4rem)] md:w-[45%] p-6 rounded-xl border border-slate-800 bg-gray-100/10 hover:border-blue-400/30 transition-all duration-200 shadow-xl">
+            <div className="w-[calc(100%-4rem)] md:w-[45%] p-5.5 rounded-xl border border-slate-800 bg-gray-100/10 hover:border-blue-400/30 transition-all duration-200 shadow-xl">
               <div className="flex flex-col sm:flex-row sm:items-center justify-between mb-1 gap-1">
-                <div className="font-bold text-slate-100 text-lg">
+                <div className="font-bold text-slate-100 text-[17px]">
                   {exp.puesto}
                 </div>
                 {/* Uso de la función formatDate y cambio a Present */}
-                <time className="font-medium text-blue-300 text-sm whitespace-nowrap mt-1 sm:mt-0">
+                <time className="font-medium text-blue-300 text-xs whitespace-nowrap mt-1 sm:mt-0">
                   {formatDate(exp.fechaInicio)} {" - "}
                   {exp.fechaFin ? formatDate(exp.fechaFin) : "Present"}
                 </time>
               </div>
-              <div className="text-blue-300 font-medium mb-2">
+              <div className="text-blue-300 font-medium text-sm mb-2">
                 {exp.empresa}
               </div>
               <div className="text-slate-400 text-sm leading-relaxed">
