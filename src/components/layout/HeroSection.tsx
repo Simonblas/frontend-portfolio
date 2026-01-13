d611d5138bc2ad0a1d37b3c281dc1bb64ba99f86
Simon <simoblas08@gmail.com>
2026-01-13 16:07:39 -0300

    Update HeroSection.tsx

--- src/components/layout/HeroSection.tsx
+++ src/components/layout/HeroSection.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 interface HeroProps {
@@ -21,10 +22,24 @@ const HeroSection = ({ nombre, titulo, cvUrl }: HeroProps) => {
     }
   };
 
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <section
       id="hero"
-      className="min-h-[90vh] flex flex-col items-center justify-center text-center px-6 bg-transparent"
+      className="min-h-[90vh] relative flex flex-col items-center justify-center text-center px-6 bg-transparent"
     >
       <h1 className="text-5xl md:text-8xl font-extrabold text-white mb-4 tracking-tight">
         Hi, I'm <span className="text-blue-400">{nombre || "Simon"}</span>
@@ -36,7 +51,7 @@ const HeroSection = ({ nombre, titulo, cvUrl }: HeroProps) => {
       <div className="mt-10 flex gap-4 ">
         <button
           onClick={handleContactClick}
-          className="bg-blue-400 text-white px-10 py-4 rounded-full font-bold hover:bg-blue-400/30 transition shadow-lg shadow-lg text-center"
+          className="bg-blue-400 text-white px-10 py-4 rounded-full font-bold hover:bg-blue-400/50 transition shadow-lg shadow-lg text-center"
         >
           Contact Me
         </button>
@@ -58,6 +73,28 @@ const HeroSection = ({ nombre, titulo, cvUrl }: HeroProps) => {
             View CV
           </button>
         )}
+      </div>
+
+      <div
+        className={`transition-opacity duration-500 ${
+          isVisible ? "opacity-100" : "opacity-0"
+        }`}
+      >
+        <div className="absolute bottom-10 md:bottom-16 left-1/2 -translate-x-1/2">
+          <svg
+            className="w-9 h-9 text-blue-400/60 animate-iteration-count-infinite animate-float"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="1.5"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </div>
       </div>
     </section>
   );
